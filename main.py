dd58aa469e1517ec14eabbfa4bf5bcb466527880
Karam Ali <karam.13@hotmail.co.uk>
2021-05-11 23:08:28 +0100

    implemented the use of f string

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ total_bill = int(input("What was the total bill? "))
 number_of_people = int(input("How many people to split the bill? "))
 tip_percentage = int(input("What percentage tip would you like to give? "))
 split = (total_bill + ((total_bill/100)*tip_percentage))/number_of_people
-print("Each person should pay: $" + str(split))
+print(f"Each person should pay: ${split}")
