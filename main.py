ed479250991335b4770140d002612db3141ba085
Karam Ali <38656321+KaramAli@users.noreply.github.com>
2021-05-11 22:53:30 +0100

    created tip calculator and a bill splitter

---
+++ main.py
@@ -0,0 +1,6 @@
+print("Welcome to the tip calculator.")
+total_bill = int(input("What was the total bill? "))
+number_of_people = int(input("How many people to split the bill? "))
+tip_percentage = int(input("What percentage tip would you like to give? "))
+split = (total_bill + ((total_bill/100)*tip_percentage))/number_of_people
+print("Each person should pay: $" + str(split))
